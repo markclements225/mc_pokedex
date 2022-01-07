91850469e4bb2bae695d26b4890ecaffcb06e67a
markclements225 <mcleme9@gmail.com>
2022-01-07 11:15:53 -0600

    starter code for pokedex

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import PokemonList from "./PokemonList";
+import axios from "axios";
+import Pagination from "./Pagination";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    let cancel;
+    axios
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((response) => {
+        setIsLoading(false);
+        setNextPageUrl(response.data.next);
+        setPrevPageUrl(response.data.previous);
+        setPokemon(response.data.results.map((pokemon) => pokemon.name));
+      });
+
+    return () => cancel();
+  }, [currentPageUrl]);
+
+  function nextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+
+  function prevPage() {
+    setCurrentPageUrl(prevPageUrl);
+  }
+
+  if (isLoading) return "Loading...";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        nextPage={nextPageUrl ? nextPage : null}
+        prevPage={prevPageUrl ? prevPage : null}
+      />
+    </>
   );
 }
 
